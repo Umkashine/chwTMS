5844fa8ee5b4df69e88b8daaf2ba75be0bb32d33
Umkashine <dimavmide@gmail.com>
2023-09-02 11:33:08 +0300

    Задача 2: В переменной n хранится двузначное число. Создайте программу, вычисляющую и выводящую на экран сумму цифр n. Например: n =26, в результате мы должны получить 8 (2+6).

---
+++ Work2/Homework2/Task2.java
@@ -0,0 +1,11 @@
+package Work2.Homework2;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int n = 26;
+        int x = n / 10;
+        int o = n % 10;
+        System.out.println(x + o);
+    }
+}
+
