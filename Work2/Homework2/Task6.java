4167ce551941cbe655d6964272068d5f6a3f998f
Umkashine <dimavmide@gmail.com>
2023-09-02 11:41:40 +0300

    Задача *: Написать программу которая будет менять местами значение целочисленных переменных. Пример: int a = 1; int b = 2; //код (ваше решение) sout(a); //выведет 2 sout(b); //выведет 1

---
+++ Work2/Homework2/Task6.java
@@ -0,0 +1,14 @@
+package Work2.Homework2;
+
+public class Task6 {
+        public static void main(String[] args) {
+            int a = 1;
+            int b = 2;
+            int temp = a;
+            a = b;
+            b = temp;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(a + "," + b);
+        }
+    }
