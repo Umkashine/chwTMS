85deb1e3858cf18848740eec22d810afacbc7c6f
Umkashine <dimavmide@gmail.com>
2023-09-02 11:29:15 +0300

    Задача 1: Написать приложение, которое будет вычислять и выводить значение по формуле: a=4*(b+c-1)/2; b и c задаем в коде самостоятельно.

---
+++ Work2/Homework2/Task1.java
@@ -0,0 +1,11 @@
+package Work2.Homework2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int b = 10;
+        int c = 20;
+        int a = 4 * (b + c - 1) / 2;
+        System.out.println(a);
+    }
+}
+
