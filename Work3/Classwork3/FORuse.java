935dd43b30c9e0cd606575891d608f6c7701093a
Umkashine <dimavmide@gmail.com>
2023-09-02 13:27:35 +0300

    Задача 3: Используя for вывести каждое четное число от 2 до 20 включительно и больше 10

---
+++ Work3/Classwork3/FORuse.java
@@ -0,0 +1,10 @@
+package Work3.Classwork3;
+
+public class FORuse {
+    public static void main (String [] args){
+        for(int a = 2 ; a <= 20 ; a +=2){
+            if (a >= 10)
+        System.out.println(a + " ");
+        }
+    }
+}
