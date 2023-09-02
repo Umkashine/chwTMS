66c13e4e09e46b01eb4314a3484df3422e69297b
Umkashine <dimavmide@gmail.com>
2023-09-02 12:13:46 +0300

    Задача 2: Используя while вывести все числа от 0 до 25 в одну строку через пробел.

---
+++ Work3/Classwork3/While25.java
@@ -0,0 +1,11 @@
+package Work3.Classwork3;
+
+public class While25 {
+    public static void main (String[] args) {
+        int a = 0;
+        while (a  < 25 ){
+            a++;
+            System.out.print(a +" " );
+        }
+    }
+}
