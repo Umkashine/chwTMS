47172ac04303cc0206935d801d28e74de1c713bb
Umkashine <dimavmide@gmail.com>
2023-09-02 11:59:18 +0300

    Задача 1: Ввести с консоли любое число от 1 до 12. В зависимости от введённого числа вывести в консоль соответствующую пору года по номеру месяца. При решении использовать switch.

---
+++ Work3/Classwork3/TimeOFtheYEAR.java
@@ -0,0 +1,61 @@
+package Work3.Classwork3;
+
+import java.util.Scanner;
+
+public class TimeOFtheYEAR {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner (System.in);
+        int a = Scanner.nextInt();
+        switch (a){
+            case 1: {
+                System.out.println("yanvar");
+                break;
+            }
+            case 2: {
+                System.out.println("fevral");
+                break;
+            }
+            case 3: {
+                System.out.println("mart");
+                break;
+            }
+            case 4: {
+                System.out.println("april");
+                break;
+            }
+            case 5: {
+                System.out.println("may");
+                break;
+            }
+            case 6: {
+                System.out.println("iyn");
+                break;
+            }
+            case 7: {
+                System.out.println("yul");
+                break;
+            }
+            case 8: {
+                System.out.println("august");
+                break;
+            }
+            case 9: {
+                System.out.println("september");
+                break;
+            }
+            case 10: {
+                System.out.println("october");
+                break;
+            }
+            case 11: {
+                System.out.println("november");
+                break;
+            }
+            case 12: {
+                System.out.println("december");
+                break;
+            }
+        }
+    }
+}
+
