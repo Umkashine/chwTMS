8db9508be31ad917542675830977cec2a0a7f81a
Umkashine <dimavmide@gmail.com>
2023-09-02 11:37:59 +0300

    Задача 4: В переменной n хранится вещественное число с ненулевой дробной частью. Создайте программу, округляющую число n до ближайшего целого и выводящую результат на экран.

---
+++ Work2/Homework2/Task4.java
@@ -0,0 +1,9 @@
+package Work2.Homework2;
+
+public class Task4 {
+    public static void main(String[] args) {
+        double n = 11.77;
+        int result = (int)Math.round(n);
+        System.out.println(result);
+    }
+}
