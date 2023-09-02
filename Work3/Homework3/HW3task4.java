c6915756bb63c887a2ffd39f9e0252c73cbce1d5
Umkashine <dimavmide@gmail.com>
2023-09-02 23:14:04 +0300

    Задача 4: Необходимо, чтоб программа выводила на экран вот такую последовательность: 7 14 21 28 35 42 49 56 63 70 77 84 91 98. В решении используйте цикл while.

---
+++ Work3/Homework3/HW3task4.java
@@ -0,0 +1,11 @@
+package Work3.Homework3;
+
+public class HW3task4 {
+    public static void main (String [] args) {
+        int a = 1;
+        while (a <= 14) {
+         System.out.print(a*7 +" ");
+         a++;
+        }
+    }
+}
