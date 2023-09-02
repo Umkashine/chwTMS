3c6bede43bb4195cac2eaacae9aee066ef783c81
Umkashine <dimavmide@gmail.com>
2023-09-02 23:02:08 +0300

    Задача 3: Составьте программу, выводящую на экран квадраты чисел от 10 до 20 включительно.

---
+++ Work3/Homework3/HW3task3.java
@@ -0,0 +1,11 @@
+package Work3.Homework3;
+
+public class HW3task3 {
+    public static void main (String [] args) {
+        int x = 10;
+        while ( x <= 20) {
+            System.out.print(x*x +" ");
+            x++;
+        }
+    }
+}
